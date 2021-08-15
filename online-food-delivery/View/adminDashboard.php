5e7eb1b8829b179bb6517fcf1fb34700debd67d8
JotyGitAccount <85220503+JotyGitAccount@users.noreply.github.com>
2021-08-16 01:14:37 +0600

    Add files via upload

---
+++ online-food-delivery/View/adminDashboard.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    
+    <link rel="stylesheet" type="text/css" href="../assests/style.css">
+</head>
+<body>
+	<table width="100%" cellspacing="0">
+		<tr>
+            <td align="right" class="hf">
+                <h1 align="left" style="color: cornsilk">Online Food Delivery</h1>
+                                     <a href="../../index.html" class="a1">Logout</a>
+            </td>
+        </tr>
+		<tr height ="500px">
+			<td align="left" width="500px"><h3 align="center" style="color: blue">Logged in as admin</h3>
+			<hr size="1" style="color: cyan">
+				<ul>
+					<li><a href="adminDashboard.php" >Dashboard</a></li>
+					<li><a href="viewprofile.php" >View Profile</a></li>
+					<li><a href="editprofile.php">Edit Profile</a></li>
+					<li><a href="displayAdmin.php">Display Admin</a></li>
+					<li><a href="menu.php">Menu</a></li>
+					<li><a href="payment-list.php">Payment List</a></li>
+					<li><a href="customer-list.php">Customer List</a></li>
+					
+					<li><a href="../Controller/logout.php">Logout</a></li>
+				</ul>
+			</td>
+		</tr>
+		<tr height = "30px" class="hf">
+            <td colspan="2">
+                <center> Thank Your For Stay with us.Stay Good And Stay Safe </center>
+            </td>
+        </tr>
+		
+	</table>
+</body>
+</html>
